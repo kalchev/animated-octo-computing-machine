4e3e566e24b24354ecfde0777bebc779c8b11cd4
kalchev <kaloyan.kalchev@gmail.com>
2016-07-12 11:38:24 +0300

    add file

---
+++ New Folder/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace animated_octo_computing_machine
+{
+    public class Class1
+    {
+        
+    }
+}
